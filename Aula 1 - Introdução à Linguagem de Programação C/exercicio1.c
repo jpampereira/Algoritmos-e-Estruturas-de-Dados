33e639d1fe783cbb2f98b9113dbdf9df2bf78124
João Pedro de Abreu Martins Pereira <jpampereira@gmail.com>
2020-09-11 02:02:51 -0300

    Update exercicio1.c

--- Aula 1 - Introdução à Linguagem de Programação C/exercicio1.c
+++ Aula 1 - Introdução à Linguagem de Programação C/exercicio1.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
 void impares(int n) {
-    int cont = 0, i = 1;
-
-    while(cont < n) {
-        if(i%2 != 0) {
-            if(cont < n-1) {
-                printf("%d ", i);
-                cont++;   
-            } else {
-                printf("%d\n", i);
-                cont++;   
-            }
-        }
-        i++;
+    int cont = 0;
+    
+    for(int i = 1; cont < n; i += 2) {   
+        if(cont < n-1) printf("%d ", i);   
+        else printf("%d\n", i);  
+        
+        cont++;    
     }
 }
 

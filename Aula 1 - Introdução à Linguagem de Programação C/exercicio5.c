36f204309691e775f2f71a2add47b327ea8373bd
João Pedro de Abreu Martins Pereira <jpampereira@gmail.com>
2020-09-11 02:30:58 -0300

    Update exercicio5.c

--- Aula 1 - Introdução à Linguagem de Programação C/exercicio5.c
+++ Aula 1 - Introdução à Linguagem de Programação C/exercicio5.c
@@ -1,42 +1,19 @@
-/*
- * Exercício: Inverte sequência de números
- *
- * Escreva um programa que recebe uma lista de números inteiros  e inverte os elementos dessa lista.
- *
- * A entrada de dados conterá um número natural positivo n, que representa a quantidade de números da sequência. 
- * Após esse número, serão fornecidos os n números (inteiros quaisquer) propriamente ditos.
- *
- * Exemplo de teste:
- * 
- * Entrada:
- * 3
- * 
- * 1
- * 2
- * 3
- * 
- * Saída:
- * 3 2 1
- */
-
 #include <stdio.h>
 
 int main() {
     int n = 0, i = 0, aux = 0;
     
-    // Quantidade de números da sequência
-    printf("Entrada:\n");
+    // Quantidade de numeros da sequencia
     scanf("%d", &n);
 
     int numeros[n];
 
-    // Recebendo os números da sequência
-    printf("\n");
+    // Recebendo os numeros da sequencia
     for(i = 0; i < n; i++) {
         scanf("%d", &numeros[i]);
     }
 
-    // Invertendo a posição dos números
+    // Invertendo a posicao dos numeros
     for(i = 0; i < n/2; i++) {
         if(i != n-i-1) {
             aux = numeros[i];
@@ -45,14 +22,10 @@ int main() {
         }
     }
 
-    // Imprimindo sequência de números invertida
-    printf("\nSaída:\n");
+    // Imprimindo sequencia de numeros invertida
     for(i = 0; i < n; i++) {
-        if(i < n-1) {
-            printf("%d ", numeros[i]);
-        } else {
-            printf("%d\n", numeros[i]);
-        }
+        if(i < n-1) printf("%d ", numeros[i]);
+        else printf("%d\n", numeros[i]);
     }
 
     return 0;

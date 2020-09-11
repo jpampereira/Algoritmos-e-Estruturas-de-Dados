40710f1ba2bf1d43df3f44452f7ce9407e2d684e
João Pedro de Abreu Martins Pereira <jpampereira@gmail.com>
2020-09-11 02:38:37 -0300

    Update exercicio6.c

--- Aula 1 - Introdução à Linguagem de Programação C/exercicio6.c
+++ Aula 1 - Introdução à Linguagem de Programação C/exercicio6.c
@@ -1,29 +1,9 @@
-/*
- * Exercício: Soma Abaixo da Diagonal
- *
- * Escreva uma função que recebe uma matriz quadrada de inteiros de dimensão n. 
- * Seu programa deve calcular e devolver a soma dos elementos abaixo da diagonal principal (excluindo a diagonal principal).
- * 
- * Exemplo de teste:
- *
- * Entrada:
- * 3
- *
- * 0 1 0
- * 2 4 7
- * 1 3 5
- * 
- * Saída:
- * 6
- */
-
 #include <stdio.h>
 
 int main() {
     int n = 0, soma = 0;
 
     // Tamanho da Matriz
-    printf("Entrada:\n");
     scanf("%d", &n);
 
     int matriz[n][n];
@@ -45,7 +25,6 @@ int main() {
         }
     }
 
-    printf("\nSaída:\n");
     printf("%d\n", soma);
     return 0;
 }
